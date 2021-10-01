715be5733bba15e0cf29097a959d0c9a516db57c
Roderick Trice <rocky_trice@yahoo.com>
2021-10-01 15:19:53 -0400

    Adding the header

---
+++ src/Components/Header_footer/header.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { AppBar, Toolbar, Button } from "@mui/material";
+import "../../Resources/css/app.css";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <AppBar
+      position="fixed"
+      style={{
+        backgroundColor: "#98c5e9",
+        boxShadow: "none",
+        padding: "10px 0",
+        borderBottom: "2px solid #00285e",
+      }}
+    >
+      <Toolbar style={{ display: "flex" }}>
+        <div style={{ flexGrow: 1 }}>
+          <div className="header_logo">
+            {/* passing the props from the icon component */}
+            {/* <CityLogo link={true} linkTo="/" width="70px" height="70px" /> */}
+          </div>
+        </div>
+        <Link to="/the_team">
+          <Button color="inherit">The Team</Button>
+        </Link>
+        <Link to="/matches">
+          <Button color="inherit">Matches</Button>
+        </Link>
+        <Link to="/dashboard">
+          <Button color="inherit">Dashboard</Button>
+        </Link>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
